6c3ce9c56041b325c462e42be37e582161398220
jaredcast <jaredcast97@gmail.com>
2021-01-21 16:13:53 -0800

    test

--- src/Photo.js
+++ src/Photo.js
@@ -10,7 +10,15 @@ const handleDragStart = (e) => e.preventDefault();
 const items = [
     { id:1,image: "/1.jpg"},
     {id:2,image: "/2.jpg"},
-    { id:3, image: "/3.jpg"}
+    { id:3, image: "/3.jpg"},
+    { id:4, image: "/4.jpg"},
+    { id:5, image: "/5.jpg"},{ id:1,image: "/1.jpg"},
+    {id:2,image: "/2.jpg"},
+    { id:3, image: "/3.jpg"},
+    { id:4, image: "/4.jpg"},
+    { id:5, image: "/5.jpg"},   
+    //{ id:3, image: "/3.jpg"},
+    
   ];
 
 const Photo = () => {
